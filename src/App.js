78a67c1f6d68311fac45a934903c89cf64287240
leostelon <53221136+leostelon@users.noreply.github.com>
2023-04-25 12:16:13 +0530

    added welcome screen

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import { WelcomeScreen } from './screens/Welcome';
 
 function App() {
+  const [isWelcomeScreen, setIsWelcomeScreen] = useState(false);
+
+  function onCloseWelcome() {
+    setIsWelcomeScreen(false);
+  }
+
+  useEffect(() => {
+    const isWelcome = localStorage.getItem("welcome");
+    if (isWelcome !== "true") {
+      setIsWelcomeScreen(true);
+    }
+  }, [])
+
+
   return (
     <div className="App">
-      <p>
+      {isWelcomeScreen ? (
+        <WelcomeScreen onCloseWelcome={onCloseWelcome} />
+      ) : (<p>
         Hello World.
-      </p>
+      </p>)}
     </div>
   );
 }
---
+++ src/screens/Welcome.jsx
@@ -0,0 +1,71 @@
+import "./Welcome.css";
+import { Box } from "@mui/system";
+import React, { useEffect } from "react";
+import PlaneScult from "../assets/planesculpt.png";
+import { IoChevronForwardOutline } from "react-icons/io5";
+
+export const WelcomeScreen = ({ onCloseWelcome }) => {
+	useEffect(() => {
+		localStorage.setItem("welcome", true);
+	}, []);
+
+	return (
+		<Box
+			sx={{
+				position: "relative",
+				height: "100vh",
+				width: "100vw",
+				maxWidth: "100vw",
+				overflow: "hidden",
+			}}
+		>
+			<p className="emoji-design unicorn">🦄</p>
+			<p className="emoji-design rocket">🔐</p>
+			<p className="emoji-design heart">💖</p>
+			<Box
+				p={3}
+				sx={{ display: "flex", flexDirection: "column", height: "100%" }}
+			>
+				<Box position={"relative"} className="content-container">
+					{/* <Box className="gradient-container"></Box> */}
+					<Box className="main-title">
+						<p>Decentralized</p>
+						<p>Docker Registry on</p>
+						<p>
+							<span className="nft-title">Spheron & Polybase</span>
+						</p>
+					</Box>
+					<Box className="welcome-button-container">
+						<Box className="continue-button" onClick={onCloseWelcome}>
+							<p>Continue</p>
+							<Box pr={1}></Box>
+							<IoChevronForwardOutline />
+						</Box>
+						<p className="continue-button-subtitle">
+							*Built in a hurry, things might brake😄
+						</p>
+					</Box>
+				</Box>
+			</Box>
+
+			{/* Image */}
+			<Box
+				sx={{
+					position: "absolute",
+					bottom: { xs: -10, md: -120 },
+					width: "100vw",
+				}}
+				className="planesculpt"
+			>
+				<img
+					style={{
+						// width: "110vw",
+						width: "100%",
+					}}
+					src={PlaneScult}
+					alt="planescultp"
+				/>
+			</Box>
+		</Box>
+	);
+};
