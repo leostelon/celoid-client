2088225e0c12a7dc37ac3c147c3b10e820dd1f0b
oBLAZERo2001 <hari9112001@gmail.com>
2023-06-07 23:58:06 +0530

    add link design completed

--- src/Components/AddLink.jsx
+++ src/Components/AddLink.jsx
@@ -1,4 +1,4 @@
-import { Box, Dialog } from "@mui/material";
+import { Box, Dialog, FormControl, TextField } from "@mui/material";
 import React, { useEffect, useState } from "react";
 
 export default function AddLink({ isOpen, handleExternalClose }) {
@@ -20,7 +20,27 @@ export default function AddLink({ isOpen, handleExternalClose }) {
   return (
     <div>
       <Dialog open={open} minWidth="md" onClose={handleClose}>
-        <Box>opened</Box>
+        <Box
+          sx={{
+            p: 2,
+            textAlign: "center",
+            width: "100%",
+            maxWidth: "350px",
+          }}
+        >
+          <h2>Add New Link</h2>
+          <FormControl fullWidth sx={{ mt: 2 }}>
+            <TextField
+              id="LinkTitle"
+              label="Title"
+              variant="outlined"
+              size="small"
+            />
+          </FormControl>
+          <FormControl fullWidth sx={{ mt: 2 }}>
+            <TextField id="Link" label="Link" variant="outlined" size="small" />
+          </FormControl>
+        </Box>
       </Dialog>
     </div>
   );
