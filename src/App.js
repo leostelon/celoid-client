5526844c985d04fbd55af7b8c602b6b42a1b2a5a
oBLAZERo2001 <hari9112001@gmail.com>
2023-06-07 10:21:26 +0530

    Profile intorduced

--- src/App.js
+++ src/App.js
@@ -1,67 +1,17 @@
 import "./App.css";
 import "./styles/navbar.css";
 
-// import { Box } from "@mui/material";
-// import { useEffect, useState } from "react";
-// import { WelcomeScreen } from "./screens/Welcome";
-// import { connectWalletToSite, getWalletAddress } from "./utils/wallet";
-// import { BsPerson } from "react-icons/bs";
-// import { createUser } from "./api/user";
 import Main from "./screens/Main";
+import Profile from "./screens/Profile";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  // const [connectedToSite, setConnectedToSite] = useState(false);
-  // const [isWelcomeScreen, setIsWelcomeScreen] = useState(false);
-
-  // async function connectSite() {
-  //   await connectWalletToSite();
-  //   const address = await getWalletAddress();
-  //   if (address && address !== "") {
-  //     const token = localStorage.getItem("token");
-  //     if (!token || token === "") {
-  //       await createUser(address);
-  //       setConnectedToSite(true);
-  //     }
-  //   }
-  // }
-
-  // function onCloseWelcome() {
-  //   setIsWelcomeScreen(false);
-  // }
-
-  // useEffect(() => {
-  //   const isWelcome = localStorage.getItem("welcome");
-  //   if (isWelcome !== "true") {
-  //     setIsWelcomeScreen(true);
-  //   }
-  //   connectSite();
-  // }, []);
-
   return (
     <div className="App">
-      <Main />
-
-      {/* {isWelcomeScreen ? (
-        <WelcomeScreen onCloseWelcome={onCloseWelcome} />
-      ) : (<div className="App">
-        <div className="navbar">
-          <div>
-            <h2>⚡Dedocker</h2>
-          </div>
-          <div
-            style={{
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "center",
-            }}
-          >
-            {!connectedToSite ? <Box onClick={connectSite} className="upload-button">
-              Connect Wallet
-            </Box> : <Box className="profile-icon">
-              <BsPerson size={30} /> </Box>}
-          </div>
-        </div>
-      </div>)} */}
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/screens/Profile.jsx
@@ -0,0 +1,181 @@
+import {
+  Box,
+  Button,
+  IconButton,
+  InputBase,
+  Switch,
+  styled,
+} from "@mui/material";
+
+import { MdDeleteOutline, MdOutlineImage } from "react-icons/md";
+import { AiOutlineDelete, AiOutlinePlus } from "react-icons/ai";
+import { deepPurple, purple } from "@mui/material/colors";
+
+export default function Profile() {
+  return (
+    <div>
+      <MainContainer>
+        <Holder>
+          <ColorButton
+            variant="contained"
+            startIcon={<AiOutlinePlus />}
+            sx={{}}
+          >
+            Add Link
+          </ColorButton>
+          {[1, 2, 3].map((a) => (
+            <LinkCont key={a}>
+              <LinkDescription>
+                <Box sx={{ paddingLeft: "10px" }}>
+                  <Box>
+                    <InputBase
+                      sx={{
+                        fontSize: "16px",
+                        fontWeight: "bold",
+                      }}
+                      value={"Twitter"}
+                      inputProps={{ "aria-label": "Twitter" }}
+                    />
+                  </Box>
+                  <Box sx={{ fontSize: "16px" }}>
+                    <InputBase
+                      sx={{
+                        fontSize: "16px",
+                      }}
+                      value={"https://twitter.com/leostelon"}
+                      inputProps={{ "aria-label": "Twitter" }}
+                    />
+                  </Box>
+                </Box>
+                <Box>
+                  <IOSSwitch defaultChecked />
+                </Box>
+              </LinkDescription>
+              <LinkDescription>
+                <Box sx={{ display: "flex", alignItems: "center" }}>
+                  <IconButton aria-label="delete">
+                    <MdOutlineImage />
+                  </IconButton>
+                  <Box>0 clicks</Box>
+                </Box>
+                <Box>
+                  <IconButton aria-label="delete">
+                    <MdDeleteOutline />
+                  </IconButton>
+                </Box>
+              </LinkDescription>
+            </LinkCont>
+          ))}
+        </Holder>
+      </MainContainer>
+    </div>
+  );
+}
+
+const MainContainer = styled(Box)({
+  //   backgroundColor: "#ff5f2e",
+  backgroundColor: "#f3f3f1",
+  width: "100%",
+
+  padding: " 0 10%",
+
+  display: "flex",
+  justifyContent: "center",
+});
+
+const Holder = styled(Box)({
+  maxWidth: "990px",
+
+  width: "80%",
+  "@media only screen and (max-width: 400px)": {
+    width: "99%",
+  },
+
+  //   padding: "20px",
+
+  "@media only screen and (max-width: 335px)": {
+    padding: "5px",
+  },
+});
+
+const LinkCont = styled(Box)({
+  width: "100%",
+  height: "140px",
+  backgroundColor: "#ffffff",
+
+  borderRadius: "20px",
+  padding: "20px",
+
+  marginBottom: "16px",
+});
+const LinkDescription = styled(Box)({
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  flexWrap: "wrap",
+});
+
+const ColorButton = styled(Button)(({ theme }) => ({
+  color: theme.palette.getContrastText(purple[500]),
+  backgroundColor: deepPurple[500],
+  "&:hover": {
+    backgroundColor: deepPurple[700],
+  },
+  width: "100%",
+  margin: "16px 0",
+  borderRadius: "28px",
+  padding: "12px",
+  fontSize: "16px",
+  fontWeight: "600",
+}));
+
+const IOSSwitch = styled((props) => (
+  <Switch focusVisibleClassName=".Mui-focusVisible" disableRipple {...props} />
+))(({ theme }) => ({
+  width: 42,
+  height: 26,
+  padding: 0,
+  "& .MuiSwitch-switchBase": {
+    padding: 0,
+    margin: 2,
+    transitionDuration: "300ms",
+    "&.Mui-checked": {
+      transform: "translateX(16px)",
+      color: "#fff",
+      "& + .MuiSwitch-track": {
+        backgroundColor: theme.palette.mode === "dark" ? "#2ECA45" : "#65C466",
+        opacity: 1,
+        border: 0,
+      },
+      "&.Mui-disabled + .MuiSwitch-track": {
+        opacity: 0.5,
+      },
+    },
+    "&.Mui-focusVisible .MuiSwitch-thumb": {
+      color: "#33cf4d",
+      border: "6px solid #fff",
+    },
+    "&.Mui-disabled .MuiSwitch-thumb": {
+      color:
+        theme.palette.mode === "light"
+          ? theme.palette.grey[100]
+          : theme.palette.grey[600],
+    },
+    "&.Mui-disabled + .MuiSwitch-track": {
+      opacity: theme.palette.mode === "light" ? 0.7 : 0.3,
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    boxSizing: "border-box",
+    width: 22,
+    height: 22,
+  },
+  "& .MuiSwitch-track": {
+    borderRadius: 26 / 2,
+    backgroundColor: theme.palette.mode === "light" ? "#E9E9EA" : "#39393D",
+    opacity: 1,
+    transition: theme.transitions.create(["background-color"], {
+      duration: 500,
+    }),
+  },
+}));
