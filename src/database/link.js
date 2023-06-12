90e20f2bdc9a36ee6f88eac3a3bdca64ab14276f
leostelon <53221136+leostelon@users.noreply.github.com>
2023-06-12 10:07:18 +0530

    add new link

---
+++ src/database/link.js
@@ -0,0 +1,50 @@
+import { pinFileToIPFS } from "../api/nftStorage";
+
+const { db } = require(".");
+
+const collectionReference = db.collection("Link");
+
+export async function createLink(url, title) {
+    try {
+        const address = localStorage.getItem("address");
+
+        const response = await collectionReference.create([
+            url, title, address
+        ]);
+
+        return response.data;
+    } catch (err) {
+        console.log(err.message)
+    }
+}
+
+export async function updateImage(id, image) {
+    try {
+        const imageUrl = await pinFileToIPFS(image)
+        const response = await collectionReference.record(id).call("updateImage", [imageUrl]);
+
+        return response;
+    } catch (err) {
+        console.log(err.message)
+    }
+}
+
+export async function updateUrl(id, url) {
+    try {
+        const response = await collectionReference.record(id).call("updateURL", [url]);
+
+        return response;
+    } catch (err) {
+        console.log(err.message)
+    }
+}
+
+export async function updateTitle(id, title) {
+    try {
+        const response = await collectionReference.record(id).call("updateTitle", [title]);
+
+        return response;
+    } catch (err) {
+        console.log(err.message)
+    }
+}
